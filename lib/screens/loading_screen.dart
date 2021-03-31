3e61eb26928cb4064a01654af526cf1ab28ffc0c
Nilesh <allmailtonilesh@gmail.com>
2021-03-31 23:47:00 +0530

    Add loader icon

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:weather_app/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:weather_app/services/location.dart';
-import 'dart:convert';
+import 'package:weather_app/services/networking.dart';
+
+const apiKey = '19fba74d5e07bf0095e6f6b26f58ed94';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,39 +12,40 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  double latitude;
+  double longitude;
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    getLocationData();
   }
 
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
-  }
+    latitude = location.latitude;
+    longitude = location.longitude;
 
-  void getData() async {
-    http.Response response = await http.get(Uri.parse(
-        'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=19fba74d5e07bf0095e6f6b26f58ed94'));
-    if (response.statusCode == 200) {
-      String data = response.body;
-      var decodedData = jsonDecode(data);
-      int condition = decodedData(data)['weather'][0]['id'];
-      print(condition);
-      double temp = decodedData(data)['main']['temp'];
-      print(temp);
-      String city = decodedData(data)['name'];
-      print(city);
-    } else {
-      print(response.statusCode);
-    }
+    NetworkHelper networkHelper = NetworkHelper(
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+
+    var weatherData = await networkHelper.getData();
+
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    getData();
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitWanderingCubes(
+          color: Colors.white,
+          size: 120.0,
+        ),
+      ),
+    );
   }
 }
