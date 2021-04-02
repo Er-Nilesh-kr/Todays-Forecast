e3b4c5cc0c44c2bb1dcab795a47d6cd15b60c584
Nilesh <allmailtonilesh@gmail.com>
2021-04-02 14:40:19 +0530

    Refactor location

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:weather_app/screens/city_screen.dart';
 import 'package:weather_app/utilities/constants.dart';
 import 'package:weather_app/services/weather.dart';
 
@@ -25,7 +27,14 @@ class _LocationScreenState extends State<LocationScreen> {
 
   void updateUI(dynamic weatherData) {
     setState(() {
-      double temp = weatherData['main']['temp'];
+      if (weatherData == null) {
+        temperature = 0;
+        weatherIcon = 'Error';
+        weatherMessage = 'Unable to get the location';
+        cityName = '';
+        return;
+      }
+      double temp = weatherData['main']['temp'].toDouble();
       temperature = temp.toInt();
       var condition = weatherData['weather'][0]['id'];
       weatherIcon = weather.getWeatherIcon(condition);
@@ -56,16 +65,31 @@ class _LocationScreenState extends State<LocationScreen> {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
+                    },
                     child: Icon(
-                      Icons.near_me,
+                      CupertinoIcons.placemark,
                       size: 50.0,
                     ),
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () async {
+                      var typedName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) {
+                          return CityScreen();
+                        }),
+                      );
+                      if (typedName != null) {
+                        var weatherData =
+                            await weather.getCityWeather(typedName);
+                        updateUI(weatherData);
+                      }
+                    },
                     child: Icon(
-                      Icons.location_city,
+                      CupertinoIcons.search_circle,
                       size: 50.0,
                     ),
                   ),
@@ -87,10 +111,12 @@ class _LocationScreenState extends State<LocationScreen> {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(right: 15.0),
+                padding: EdgeInsets.all(15.0),
                 child: Text(
-                  '$weatherMessage in $cityName!',
-                  textAlign: TextAlign.right,
+                  cityName == ''
+                      ? 'Unable to find the location'
+                      : '$weatherMessage\nin\n$cityName!',
+                  textAlign: TextAlign.center,
                   style: kMessageTextStyle,
                 ),
               ),
