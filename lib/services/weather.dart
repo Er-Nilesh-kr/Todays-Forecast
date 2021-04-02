25d93e602b701422c39fe965b6d9818912d15bc0
Nilesh <allmailtonilesh@gmail.com>
2021-04-02 14:41:18 +0530

    Add getCityWeather functionality

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,28 @@
+import 'package:weather_app/services/location.dart';
+import 'package:weather_app/services/networking.dart';
+
+const apiKey = '19fba74d5e07bf0095e6f6b26f58ed94';
+const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper =
+        NetworkHelper('$openWeatherUrl?q=$cityName&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherUrl?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return (weatherData);
+  }
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
