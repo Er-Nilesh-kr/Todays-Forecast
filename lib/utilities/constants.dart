c567ac9651480a97e356f2a39b9f1ef01a79b0f5
Nilesh <allmailtonilesh@gmail.com>
2021-04-02 14:41:49 +0530

    Add TextInputDecoration styling

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -7,7 +7,7 @@ const kTempTextStyle = TextStyle(
 
 const kMessageTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 60.0,
+  fontSize: 44.0,
 );
 
 const kButtonTextStyle = TextStyle(
@@ -17,4 +17,23 @@ const kButtonTextStyle = TextStyle(
 
 const kConditionTextStyle = TextStyle(
   fontSize: 100.0,
+);
+
+const kTextFieldInputDecoration = InputDecoration(
+  filled: true,
+  fillColor: Colors.white70,
+  icon: Icon(
+    Icons.location_city_rounded,
+    size: 40.0,
+    color: Colors.white,
+  ),
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(7.0),
+    ),
+  ),
 );
