692a835a5ccc7a5542129aee7e9bac367e6bf69e
Nilesh <allmailtonilesh@gmail.com>
2021-04-05 13:00:21 +0530

    Fix UI

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const kTempTextStyle = TextStyle(
   fontFamily: 'Spartan MB',
-  fontSize: 100.0,
+  fontSize: 90.0,
 );
 
 const kMessageTextStyle = TextStyle(
@@ -16,7 +16,7 @@ const kButtonTextStyle = TextStyle(
 );
 
 const kConditionTextStyle = TextStyle(
-  fontSize: 100.0,
+  fontSize: 90.0,
 );
 
 const kTextFieldInputDecoration = InputDecoration(
