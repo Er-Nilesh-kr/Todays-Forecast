3aeab9c23c2e6345fc42c49a09925c545e4e3979
Nilesh <allmailtonilesh@gmail.com>
2021-04-05 13:00:11 +0530

    Add AQI & Humidity level data

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -3,6 +3,9 @@ import 'package:weather_app/services/networking.dart';
 
 const apiKey = '19fba74d5e07bf0095e6f6b26f58ed94';
 const openWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const airQualityUrl = 'https://api.openweathermap.org/data/2.5/air_pollution';
+
+//?lat=50&lon=50&appid=19fba74d5e07bf0095e6f6b26f58ed94
 
 class WeatherModel {
   Future<dynamic> getCityWeather(String cityName) async {
@@ -10,6 +13,28 @@ class WeatherModel {
         NetworkHelper('$openWeatherUrl?q=$cityName&appid=$apiKey&units=metric');
     var weatherData = await networkHelper.getData();
     return weatherData;
+  }
+
+  Future<dynamic> getCurrentAQI() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper(
+        '$airQualityUrl?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey');
+
+    var aqiData = await networkHelper.getData();
+    return (aqiData);
+  }
+
+  Future<dynamic> getCityAQI(double latit, double longi) async {
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper =
+        NetworkHelper('$airQualityUrl?lat=${latit}&lon=${longi}&appid=$apiKey');
+
+    var aqiData = await networkHelper.getData();
+    return (aqiData);
   }
 
   Future<dynamic> getLocationWeather() async {
@@ -54,4 +79,20 @@ class WeatherModel {
       return 'Bring a 🧥 just in case';
     }
   }
+
+  String getAQIRemark(int index) {
+    if (index == 1) {
+      return 'Good';
+    } else if (index == 2) {
+      return 'Fair';
+    } else if (index == 3) {
+      return 'Moderate';
+    } else if (index == 4) {
+      return 'Poor';
+    } else {
+      return 'Very Poor';
+    }
+  }
 }
+
+//1 = Good, 2 = Fair, 3 = Moderate, 4 = Poor, 5 = Very Poor.
